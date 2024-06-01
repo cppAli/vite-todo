75cb204ab571fc31430876f99caa5fa7927dc1d3
Alice <romankoalice@gmail.com>
2024-06-01 11:35:32 +0300

    "Remove index.jsx files in components/app and components/todo-list-item, update App import in main.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.jsx'
-import './index.css'
 import App from './components/app';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
